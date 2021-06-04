f1680511537127816977eae20737983115399ab3
mercy <84447261+faith-cpu@users.noreply.github.com>
2021-06-04 07:06:14 -0700

    Add files via upload

---
+++ adding 5 nmbers.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int a, b,c,d,e, sum;
+	
+	printf("enter the value of a");
+	scanf("%d", &a);
+	printf("enter the value of b");
+	scanf("%d", &b);
+		printf("enter the value of c");
+	scanf("%d", &c);
+	printf("enter the value of d");
+	scanf("%d", &d);
+	printf("enter the value of e");
+	scanf("%d", &e);
+
+	printf("%d", a+b+c+d+e);
+	
+	return 0;
+}
